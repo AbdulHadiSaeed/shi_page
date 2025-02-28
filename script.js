bb6b054545d3a453e19ee887c4240a6aad72db93
Abdul Hadi Saeed <abdulhadi.50@hotmail.com>
2025-02-27 19:50:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Set a timer to wait for 3 seconds after the last message, then redirect
+setTimeout(function() {
+    // Redirect to the portfolio website after 3 seconds
+    window.location.href = "https://siferry.com/"; // Replace with your actual portfolio URL
+}, 16000); // Wait for the entire animation to finish before redirecting
